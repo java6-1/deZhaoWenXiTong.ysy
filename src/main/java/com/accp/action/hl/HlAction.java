3143ce6f5149e27bfb4bfd87a9409e6b81a278ae
“He <1878064792@qq.com>
2020-07-25 14:55:48 +0800

    heling20200725下课提交

--- src/main/java/com/accp/action/hl/HlAction.java
+++ src/main/java/com/accp/action/hl/HlAction.java
@@ -7,11 +7,16 @@ import javax.servlet.http.HttpServletRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.accp.biz.hl.ChexingBiz;
+import com.accp.biz.hl.QxkzBiz;
 import com.accp.pojo.Chexing;
+import com.accp.pojo.Qxkz;
 
 
 @RestController
@@ -20,11 +25,38 @@ public class HlAction {
 	@Autowired
 	private ChexingBiz chexingBiz;
 	
+	@Autowired
+	private QxkzBiz qxkzBiz;
+	
+	
+	
+	/**查询全部**/
 	@GetMapping("/selec")
-	public List<Chexing> select() {	
-		List<Chexing> list = chexingBiz.select();
+	public List<Qxkz> select() {	
+		List<Qxkz> list = qxkzBiz.select();
 		return  list;
 	}
 	
+	/**根据ID查询**/
+	@GetMapping("/selectByPrimaryKey")
+	public Qxkz selectByPrimaryKey(Integer staffID) {	
+		Qxkz qxkz = qxkzBiz.selectByPrimaryKey(staffID);
+		return  qxkz;
+	}
+	
+	
+	/**根据ID查询**/
+	@PostMapping("/insert")
+	public int  insert(@RequestBody Qxkz record) {	
+		int count = qxkzBiz.insert(record);
+		return  count;
+	}
+	
+	/**修改会员权限**/
+	@PutMapping("/updateByPrimaryKeySelective")
+	public int  updateByPrimaryKeySelective(@RequestBody Qxkz record) {	
+		int count = qxkzBiz.updateByPrimaryKeySelective(record);
+		return  count;
+	}
 	
 }
