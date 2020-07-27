bd4d8cefaf610e7954a76fd253a39a050deee853
WuLongWang <w12101016@163.com>
2020-07-27 10:13:51 +0800

    adas

--- src/main/java/com/accp/dao/StaffMapper.java
+++ src/main/java/com/accp/dao/StaffMapper.java
@@ -1,6 +1,9 @@
 package com.accp.dao;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
 
 import com.accp.pojo.Staff;
 
@@ -16,7 +19,13 @@ public interface StaffMapper {
     int updateByPrimaryKeySelective(Staff record);
    
     int updateByPrimaryKey(Staff record);
+    //yj
     Staff selectuser(@Param("staffAccount")String staffAccount,@Param("staffPawss")String staffPawss);
+    //yj
     Staff selectuserPhone(@Param("staff")String staff);
+    
+    //查询全部wlw
+    @Select("select * from staff  INNER JOIN section ON section.`sectionbh`=staff.`sectionID`")
+    List<Staff> queryStaff();
 
 }
