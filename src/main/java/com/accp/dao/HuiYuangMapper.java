b417fb10a33cc429e2c154b8a2626cbbaa2548bc
Yang Jiang <1473356847@qq.con>
2020-08-01 09:38:21 +0800

    是

--- src/main/java/com/accp/dao/HuiYuangMapper.java
+++ src/main/java/com/accp/dao/HuiYuangMapper.java
@@ -3,15 +3,17 @@ package com.accp.dao;
 import com.accp.pojo.HuiYuang;
 
 public interface HuiYuangMapper {
-    int deleteByPrimaryKey(Integer huiyuanid);
-
-    int insert(HuiYuang record);
-
-    int insertSelective(HuiYuang record);
-
-    HuiYuang selectByPrimaryKey(Integer huiyuanid);
-
-    int updateByPrimaryKeySelective(HuiYuang record);
-
-    int updateByPrimaryKey(HuiYuang record);
+	/*
+	 * int deleteByPrimaryKey(Integer huiyuanid);
+	 * 
+	 * int insert(HuiYuang record);
+	 * 
+	 * int insertSelective(HuiYuang record);
+	 * 
+	 * HuiYuang selectByPrimaryKey(Integer huiyuanid);
+	 * 
+	 * int updateByPrimaryKeySelective(HuiYuang record);
+	 * 
+	 * int updateByPrimaryKey(HuiYuang record);
+	 */
 }
