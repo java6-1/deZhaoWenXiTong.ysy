dfa6fb8341ee7b94e4a9f80fa100a9cd482af85c
CanYou <2528474609@qq.com>
2020-08-05 16:43:22 +0800

    1

--- src/main/java/com/accp/action/zyw/Auctionghdw.java
+++ src/main/java/com/accp/action/zyw/Auctionghdw.java
@@ -2,10 +2,13 @@ package com.accp.action.zyw;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.accp.biz.zyw.Bizghdw;
+import com.accp.pojo.Clpp;
+import com.accp.pojo.Ghdw;
 
 @RestController
 @RequestMapping("/api/zyw")
@@ -17,5 +20,22 @@ public class Auctionghdw {
 	@GetMapping("/ghdwquery")
 	public Object query(Integer pageNum,String wxdz) {
 		return biz.list(pageNum, wxdz);
+	}
+	
+	@GetMapping("/ghdwdel")
+	public Object del(Integer id) {
+		biz.del(id);
+		return 1;
+	}
+	@PostMapping("/insghdw")
+	public Object insert(Ghdw ghdw) {
+	biz.ins(ghdw);
+		return 1;
+	}
+	
+	@PostMapping("/updateghdw")
+	public Object upda(Ghdw ghdw) {
+	biz.upda(ghdw);
+		return 1;
 	}
 }
--- src/main/java/com/accp/dao/GhdwMapper.java
+++ src/main/java/com/accp/dao/GhdwMapper.java
@@ -19,6 +19,6 @@ public interface GhdwMapper {
 
     int updateByPrimaryKey(Ghdw record);
     
-    @Select("SELECT * FROM `ghdw` ")
-    List<Ghdw> query(String diz);
+    @Select("<script>SELECT * FROM `ghdw` where 1=1 <if test=\" ghdwpawss !=null and ghdwpawss !=''  \">and ghdwpawss = #{ghdwpawss}</if> </script>")
+    List<Ghdw> query(String ghdwpawss);
 }
