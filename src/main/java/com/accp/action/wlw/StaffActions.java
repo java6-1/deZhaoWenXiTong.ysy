8718994fa3ae7d2882eeb87818cc9a6cdd1ac795
WuLongWang <w12101016@163.com>
2020-08-05 16:49:08 +0800

    ada

--- src/main/java/com/accp/action/wlw/StaffActions.java
+++ src/main/java/com/accp/action/wlw/StaffActions.java
@@ -127,6 +127,7 @@ public class StaffActions {
 	@GetMapping("DeStaff/{staffid}")
 	public Map<String, Object> DeStaff(@PathVariable Integer staffid){
 		int count= staffBizs.DeStaff(staffid);
+		System.out.println();
 		Map<String, Object> message = new HashMap<String, Object>();
 		message.put("code", "200");
 		message.put("msg", "ok");
